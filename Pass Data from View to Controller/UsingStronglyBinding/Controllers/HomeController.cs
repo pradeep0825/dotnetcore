08ace6b37f405c8284411f9942a54893d1714290
Pradeep.Gujjula <Pradeep.Gujjula@neudesic.com>
2022-03-25 23:52:43 +0530

    First Commit

---
+++ Pass Data from View to Controller/UsingStronglyBinding/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UsingStronglyBinding.Models;
+
+namespace UsingStronglyBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string PostUsingStronglyBinding(Employee emp)
+        {
+            string name = emp.Name;
+            int sal = emp.Salary;
+            return "Details are : " + name + ", salary is: " + sal;
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Pass Data from View to Controller/UsingStronglyBinding/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace UsingStronglyBinding.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+    }
+}
