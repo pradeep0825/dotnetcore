e0fb7d1da2dab1a116801aec72dc3bb73ebaa39b
Pradeep.Gujjula <Pradeep.Gujjula@neudesic.com>
2022-03-25 21:19:48 +0530

    Final code

---
+++ Pass Data from View to Controller/UsingRequest/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UsingRequest.Models;
+
+namespace UsingRequest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string JustChecking()
+        {
+            string myname = Request.Form["myName"];
+            return "The data we got is: " + myname;
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
