cd527db5e4ef6044f89613c2f2255fedc0f51524
Pradeep.Gujjula <Pradeep.Gujjula@neudesic.com>
2022-03-25 18:32:27 +0530

    Initial Commit

---
+++ Pass Data from View to Controller/UsingParameters/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UsingParameters.Models;
+
+namespace UsingParameters.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string JustChecking(string myName, string myAge)
+        {
+            return "The values we have got are : " + myName + ", " + myAge;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
